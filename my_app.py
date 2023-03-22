134c66c5f0c94003898c883a51e71810e25156de
BramhaChaitanya15 <128367777+BramhaChaitanya15@users.noreply.github.com>
2023-03-22 12:26:21 +0530

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -1,5 +1,13 @@
 import csv
+
+products = []
+
 with open('data/shipping_data_0.csv') as csvfile0:
   csvReader = csv.reader(csvfile0)
+  
   for row in csvReader:
-    print(row)
+    product = row[2]
+    if product not in products:
+      products.append(product)
+
+print(products)
