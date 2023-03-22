2597f79fd2c97d5c8adfa844521eff4d5811da8a
BramhaChaitanya15 <128367777+BramhaChaitanya15@users.noreply.github.com>
2023-03-22 16:23:40 +0530

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -32,22 +32,49 @@ with open('data/shipping_data_0.csv') as csvfile0:
       
 #print(shipment)
 shipmentId = []
+shiprows = {}
+skip = False
 with open('data/shipping_data_2.csv') as csvfile2:
   with open('data/shipping_data_1.csv') as csvfile1:
     csvProductReader1 = csv.reader(csvfile1)
+    tide=""
+    tanam=""
+    taqua=0
     for row in csvProductReader1:
-      if not skipped:
-        skipped = True
+      if not skip:
+        skip = True
         continue
       shipID = row[0]
       product = row[1]
       time = row[2]
       if time != "false":
+        if tide!=shipID or tanam !=product:
+          shiprow={}
+          shiprow["shipid"]=tide
+          shiprow["product"]=tanam
+          shiprow["quantity"]=taqua
+          if tide!="" and tanam!="" and taqua!=0:
+            shipmentId.append(shiprow)
+          tide=shipID
+          tanam=product
+          taqua=0
+        taqua+=1
         shipmentId.append(shipID)
         if product not in products:
           products.append(product)
           
-print(shipmentId)
+#print(shipmentId)
+    csvReader2 = csv.reader(csvfile2)
+    for shiprow in shipmentId:
+      sid=shiprow["shipid"]
+      csvfile2.seek(0)
+      for row in csvReader2:
+        shipid2 = row[0] 
+        if shipid2 == sid:
+          shiprow["origin"] = row[1]
+          shiprow["destination"] = row[2]
+      shipment.append(shiprow)
+          
 '''
     csvReader2 = csv.reader(csvfile2)
     for row in csvReader2:
@@ -65,7 +92,7 @@ for product in products:
 connection.commit()
 view = cursor.execute("SELECT * FROM product")
 print(view.fetchall())
-'''
+
 j = 1
 for shiprow in shipment:
   proID = cursor.execute("SELECT id FROM product WHERE name = '" + shiprow["product"] + "'").fetchone()
@@ -73,6 +100,6 @@ for shiprow in shipment:
                  + str(shiprow["quantity"]) + ", '" + shiprow["origin"] + "','" + shiprow["destination"] + "')")
   j+=1
   
-connection.commit()'''
-#view = cursor.execute("SELECT * FROM shipment")
-#print(view.fetchall())
+connection.commit()
+view = cursor.execute("SELECT * FROM shipment")
+print(view.fetchall())
