f0e4755121f48483165c66b04e92b2a315828c59
BramhaChaitanya15 <128367777+BramhaChaitanya15@users.noreply.github.com>
2023-03-22 12:30:40 +0530

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -4,8 +4,12 @@ products = []
 
 with open('data/shipping_data_0.csv') as csvfile0:
   csvReader = csv.reader(csvfile0)
+  skipped = False
   
   for row in csvReader:
+    if not skipped:
+      skipped = True
+      continue
     product = row[2]
     if product not in products:
       products.append(product)
